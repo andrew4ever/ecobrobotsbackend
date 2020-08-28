df574960b80187177f676229eb375cda8c93b106
Andrew Yatsura <andrewyazura203@gmail.com>
2020-08-28 21:41:35 +0300

    Add 2 types of configuration in config.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-import os
-
 from flask import Flask
 from flask_restful import Api
 
 app = Flask(__name__)
-app.config.update(
-    SECRET_KEY=os.urandom(24),
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_NAME='ecobrobotsbackend-websession'
-)
+app.config.from_object('config.DevConfig')
 
 api = Api(app)
