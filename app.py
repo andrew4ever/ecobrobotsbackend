7d7f5e1fe2ffdd1190adce0d33cd64207d7ce61e
Andrew Yatsura <andrewyazura203@gmail.com>
2020-08-29 08:36:16 +0300

    Routing resources

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
+from config import DevConfig
+from resources.area import Area
+from resources.map import Map
+
 app = Flask(__name__)
-app.config.from_object('config.DevConfig')
+app.config.from_object(DevConfig())
 
 api = Api(app)
+api.add_resource(Area, '/area' '/area/<string:square_coords>')
+api.add_resource(Map, '/map')
