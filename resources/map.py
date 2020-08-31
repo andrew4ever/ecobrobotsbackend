d9874c38be8c798662566ca6bb2276fd76c39fef
Andrew Yatsura <andrewyazura203@gmail.com>
2020-08-31 17:29:09 +0300

    Resolve #10

--- resources/map.py
+++ resources/map.py
@@ -1,14 +1,23 @@
 from flask_restful import Resource
+from sqlalchemy import desc
 
 from models import AreaModel
 
 
 class Map(Resource):
     def get(self):
-        areas = AreaModel.query.order_by(AreaModel.created).all()
+        areas = AreaModel.query.order_by(desc(AreaModel.created)).all()
 
+        areas_coords = []
         areas_list = []
-        for area in reversed(areas):
+
+        for area in areas:
+            a = area.as_dict()
+
+            if (a['latitude'], a['longitude']) in areas_coords:
+                continue
+
+            areas_coords.append((a['latitude'], a['longitude']))
             areas_list.append(area.as_dict())
 
         return areas_list
