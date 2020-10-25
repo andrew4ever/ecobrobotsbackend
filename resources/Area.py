cf75fc966f94dfaf79ea46ee34a9ea54025767ba
Andrew Yatsura <andrewyazura203@gmail.com>
2020-10-25 18:25:58 +0200

    Update query_string parsing in Area resource

--- resources/Area.py
+++ resources/Area.py
@@ -1,20 +1,37 @@
-from flask_restful import Resource, request
-from sqlalchemy import desc
+from os import environ
+from urllib.parse import parse_qs
 
+from flask_restful import Resource, request
 from models import AreaModel
+from sqlalchemy import desc
 
 
 class Area(Resource):
     def get(self):
-        latitude = request.args.get('latitude')
-        longitude = request.args.get('longitude')
+        try:
+            args = request.query_string
 
-        areas = AreaModel.query \
-            .filter_by(latitude=latitude) \
-            .filter_by(longitude=longitude) \
-            .order_by(desc(AreaModel.created)).all()
+            if not args:
+                args = environ.get('QUERY_STRING')  # if using CGI server
 
-        if not areas:
+            args = parse_qs(args).items()
+
+            try:
+                args = [(i[0].decode('utf-8'), i[1][0].decode('utf-8'))
+                        for i in args]
+            except:
+                args = [(i[0], i[1][0]) for i in args]
+
+            latitude, longitude = args[0][1], args[1][1]
+            areas = AreaModel.query \
+                .filter_by(latitude=latitude) \
+                .filter_by(longitude=longitude) \
+                .order_by(desc(AreaModel.created)).all()
+
+            if not areas:
+                return {'code': 404, 'message': 'Area not found'}, 404
+
+        except:
             return {'code': 404, 'message': 'Area not found'}, 404
 
         areas_list = []
