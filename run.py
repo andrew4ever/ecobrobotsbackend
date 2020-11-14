af657d71607dfb893abaa582b512b1d04f7f53e9
Andrew Yatsura <andrewyazura203@gmail.com>
2020-11-14 17:53:14 +0200

    Update cron job command

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ from app import create_app
 cron = CronTab(user=True)
 
 job = cron.new(
-    command='export ENVIRONMENT={1} && export PYTHONPATH={0} && cd {0} && {0}/venv/bin/python {0}/common/AQICalculator.py'.format(
-        os.getcwd(), os.environ.get('ENVIRONMENT'))
+    command='export PYTHONPATH={0} && cd $PYTHONPATH && $PYTHONPATH/venv/bin/python $PYTHONPATH/common/AQICalculator.py'.format(
+        os.getcwd())
 )
 job.minute.every(int(os.environ.get('DB_UPDATE_PERIOD_MINUTES')))
 cron.write()
