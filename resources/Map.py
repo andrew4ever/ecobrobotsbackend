f310a80cebb0b5f643823129e7546dec821f8c31
Andrew Yatsura <andrewyazura203@gmail.com>
2021-01-03 21:34:02 +0200

    Add sensor_id field to AQI records

--- resources/Map.py
+++ resources/Map.py
@@ -10,37 +10,22 @@ from sqlalchemy import desc
 class Map(Resource):
     def get(self):
         aqi_records = []
-        latest_records = self._get_latest_records()
+        sensors = SensorModel.query.all()
         maxdate = datetime.now() - timedelta(
             hours=int(environ.get('MAX_RECORD_HOURS'))
         )
 
-        for record in latest_records:
+        for sensor in sensors:
             aqi_record = AreaModel.query \
                 .order_by(
                     AreaModel.created.desc()
                 ).filter(
                     AreaModel.created >= maxdate
                 ).filter_by(
-                    latitude=record.latitude
-                ).filter_by(
-                    longitude=record.longitude
+                    sensor_id=sensor.external_id
                 ).first()
 
             if aqi_record:
                 aqi_records.append(convert_types_to_names(aqi_record))
 
         return aqi_records
-
-    def _get_latest_records(self):
-        sensors = SensorModel.query.all()
-
-        latest_records = []
-        for sensor in sensors:
-            record = SensorDataModel.query.order_by(SensorDataModel.recorded.desc()).filter_by(
-                internal_id=sensor.external_id).first()
-
-            if record:
-                latest_records.append(record)
-
-        return latest_records
